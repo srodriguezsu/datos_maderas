53042ca78871b4aa7fcceded84bcb8923e139d42
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-02-17 15:25:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -81,10 +81,10 @@ def plot_top_municipalities(df):
         if not all(col in df.columns for col in required_columns):
             st.error(f"El DataFrame no contiene las columnas necesarias: {required_columns}")
             return
-        st.write(df)
+
         # Agrupar los datos por municipio y sumar el volumen
         df_volume = df.groupby('municipio')['volumen_m3'].sum().nlargest(10).reset_index()
-
+        st.write(df_volume)
         # Cargar el archivo GeoJSON de municipios de Colombia
         url_municipios = "https://raw.githubusercontent.com/macortesgu/MGN_2021_geojson/refs/heads/main/MGN2021_MPIO_web.geo.json"
         municipios = gpd.read_file(url_municipios)
