ca48c9ed9ffc8122b891683a4bea3bb7bda2a42a
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-02-17 15:36:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,10 +84,11 @@ def plot_top_municipalities(df):
         # Agrupar los datos por municipio y sumar el volumen
         df_volume = df.groupby('municipio')['volumen_m3'].sum().nlargest(10).reset_index()
         st.write(df_volume)    
-        df['municipio'] = df['municipio'].str.upper() 
+        df_volume['municipio'] = df['municipio'].str.upper() 
         # Cargar el archivo GeoJSON de municipios de Colombia
         url_municipios = "https://raw.githubusercontent.com/macortesgu/MGN_2021_geojson/refs/heads/main/MGN2021_MPIO_web.geo.json"
-        municipios = gpd.read_file(url_municipios)
+        municipios = gpd.read_file(url_municipios)    
+        municipios['MPIO_CNMBR'] = municipios['MPIO_CNMBR'].str.upper()
 
         # Unir los datos de volumen con el GeoDataFrame de municipios
         municipios_volume = municipios.merge(df_volume, how='left', left_on='MPIO_CNMBR', right_on='municipio')
